c088ce8a4618487808e45eb0461b34c46b24b847
Santiago Ballesteros <s.ballesteros@uniandes.edu.co>
2019-11-10 18:22:54 -0500

    CREATE DE VIAJERO FUNCIONA

--- src/app/viajero/tipo-de-documento.enum.ts
+++ src/app/viajero/tipo-de-documento.enum.ts
@@ -1,5 +1,5 @@
 export enum TIPODEDOCUMENTO {
-    cedulaCiudadania = "CEDULA_DE_CIUDADANA",
+    cedulaCiudadana = "CEDULA_DE_CIUDADANA",
     cedulaExtranjeria = "CEDULA_DE_EXTRANJAERIA",
     tarjetaIdentidad = "TARJETA_DE_IDENTIDAD",
     pasaporte = "PASAPORTE"
