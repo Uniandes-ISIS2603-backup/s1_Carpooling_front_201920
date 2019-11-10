6c2a64d76fe82bac424a9bee6789976087bf24d7
Santiago Ballesteros <s.ballesteros@uniandes.edu.co>
2019-11-10 11:40:13 -0500

    detalle publicista en proceso

--- src/app/publicista/publicista-detail/publicista-detail.component.ts
+++ src/app/publicista/publicista-detail/publicista-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { PublicistaService } from '../publicista.service';
+import { Publicista } from '../publicista';
+import { PublicistaDetail } from '../publicista-detail';
 
 @Component({
   selector: 'app-publicista-detail',
@@ -7,9 +12,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicistaDetailComponent implements OnInit {
 
-  constructor() { }
+ /**
+  * The component's constructor
+  * @param editorialService The editorial's service
+  * @param route The route element which helps to obtain the editorial's id
+  * @param toastrService The toastr to show messages to the user
+  */
+ constructor(
+  private publicistaService: PublicistaService,
+  private route: ActivatedRoute
+) { }
 
-  ngOnInit() {
+/**
+* The editorial whose details we want to show
+*/
+@Input() publicistaDetail: PublicistaDetail;
+
+
+
+/**
+* The editorial's id retrieved from the address
+*/
+publicista_id: number;
+
+/**
+* The method which retrieves the publicistas of an editorial
+*/
+getPublicistaDetail(): void {
+  this.publicistaService.getPublicistaDetail(this.publicista_id)
+    .subscribe(publicistaDetail => {
+      this.publicistaDetail = publicistaDetail
+    });
+}
+
+updateCalificaciones(): void {
+  this.getPublicistaDetail();
+}
+
+/**
+* The method which initializes the component
+* We need to initialize the editorial so it is never considered as undefined
+*/
+ngOnInit() {
+  this.publicista_id = +this.route.snapshot.paramMap.get('id');
+  if (this.publicista_id) {
+    this.publicistaDetail = new PublicistaDetail();
+    this.getPublicistaDetail();
   }
 
+}
 }
