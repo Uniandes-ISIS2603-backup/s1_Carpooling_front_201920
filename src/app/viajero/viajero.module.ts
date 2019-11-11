e2585243324d6ad1e4d056d763ffe563bafd27fb
Juan David Alarcon <jd.alarcons@uniandes.edu.co>
2019-11-11 01:43:04 -0500

    Calificacion casi completo

--- src/app/viajero/viajero.module.ts
+++ src/app/viajero/viajero.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { ViajeroDetailComponent } from './viajero-detail/viajero-detail.component';
 import { ViajeroListComponent } from './viajero-list/viajero-list.component';
 import { ViajeroService } from './viajero.service';
 import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import {ViajeroCalificacionComponent} from './viajero-calificacion/viajero-calificacion.component';
+import {ViajeroAddCalificacionComponent} from './viajero-add-calificacion/viajero-add-calificacion.component'
 import { ViajeroCreateComponent } from './viajero-create/viajero-create.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   declarations: [ 
     ViajeroDetailComponent, 
     ViajeroListComponent, ViajeroCalificacionComponent,
+    ViajeroAddCalificacionComponent,
     ViajeroCreateComponent],
     providers: [ViajeroService],
     exports:[ViajeroListComponent]
